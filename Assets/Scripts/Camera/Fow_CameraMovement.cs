3826a56c158ce591e3651752bc9c71e067eb7bcd
samjerry <jerry.sam@ziggo.nl>
2020-01-30 15:24:10 +0100

    Added FOW version 2.0 (leaves a dark grey trail on no longer visible areas, Added a Camera movement script)

---
+++ Assets/Scripts/Camera/Fow_CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fow_CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float smoothTime = 0.3f;
+
+    private Vector3 goalPos;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        goalPos.x = _target.position.x;
+        goalPos.z = _target.position.z - 8;
+        goalPos.y = transform.position.y;
+
+        transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
+    }
+}
