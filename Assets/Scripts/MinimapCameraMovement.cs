04f575bd8af99edad407affcb05d93d5fd5e5d62
samjerry <jerry.sam@ziggo.nl>
2020-01-30 09:04:17 +0100

    Started on level layout

---
+++ Assets/Scripts/MinimapCameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MinimapCameraMovement : MonoBehaviour
+{
+    private Vector3 _camPos;
+
+    private float _camSpeed;
+
+    private GameObject _player;
+
+    [SerializeField] private UnityEvent OnPlayerMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camPos = transform.position;
+    }
+
+    private void MoveCamera()
+    {
+        Vector3 _newCamPos = new Vector3(_player.transform.position.x, 20, _player.transform.position.x);
+
+        _camPos = Mathf.Lerp(_camPos, _newCamPos, _camSpeed);
+    }
+}
