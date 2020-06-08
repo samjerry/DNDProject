c69cd494419c4379a68d698d7e60c8a7a9de8d9c
MarrowOfAlbion <jerry.sam@ziggo.nl>
2020-06-08 15:26:12 +0200

    Add model viewer GameObject, Rotate script and functionality, ShowModelController script

---
+++ Assets/Scripts/Model Preview/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour 
+{
+    [SerializeField]
+    private float _rotationSpeed = 15f;
+
+    private bool _isModelPreview;
+    private bool _isPlayerPreview;
+
+    private void Start() 
+    {
+        if (_isModelPreview) 
+        {
+            StartCoroutine(RotateModel());
+        }
+    }
+
+    IEnumerator RotateModel()  
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * _rotationSpeed);
+        yield return null;
+    }
+}
