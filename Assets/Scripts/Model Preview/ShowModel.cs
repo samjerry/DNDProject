60e6864dd55e947891d5f1eee2567975b87cf0a3
MarrowOfAlbion <jerry.sam@ziggo.nl>
2020-06-08 22:39:13 +0200

    Add ShowModel script

---
+++ Assets/Scripts/Model Preview/ShowModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowModel : MonoBehaviour
+{
+    [SerializeField]
+    private ShowModelButton buttonPrefab;
+
+    private void Start() 
+    {
+        var _models = FindObjectsOfType<ShowModelController>().GetModels();
+        foreach (var _model in _models) 
+        {
+            CreateButtonForModel(_model);
+        }
+    }
+
+    private void CreateButtonForModel(Transform _model) 
+    {
+
+    }
+}
