e7020ed14e5c3f89d3db06a52bbd99a2ea87ef4d
MarrowOfAlbion <jerry.sam@ziggo.nl>
2020-06-09 14:37:25 +0200

    Resolve merge conflicts. Add ShowModelButton script, Canvas, ShowModelUI Panel, ShowModelUI Button

---
+++ Assets/Scripts/Model Preview/ShowModelUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowModelUI : MonoBehaviour
+{
+    [SerializeField]
+    private ShowModelButton _buttonPrefab;
+
+    private void Start() 
+    {
+        var _models = FindObjectsOfType<ShowModelController>().GetModels();
+        foreach (var _model in _models) 
+        {
+            CreateButtonForModel(_model);
+        }
+    }
+
+    public void CreateButtonForModel(Transform _model) 
+    {
+        var button = Instantiate(_buttonPrefab);
+        button.transform.SetParent(this.transform);
+        button.transform.localScale = Vector3.one;
+        button.transform.localRotation = Quaternion.identity;
+
+        button.Initialize(_model, FindObjectOfType<ShowModelController>().EnableModel);
+    }
+}
