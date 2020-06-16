93307df0856a3ca27512ec39ac70d696f92d9973
MarrowOfAlbion <jerry.sam@ziggo.nl>
2020-06-16 11:15:55 +0200

    Add Spawner GameObject. Add MouseToWorldPos Function

--- Assets/Scripts/Creature Spawner/Spawner.cs
+++ Assets/Scripts/Creature Spawner/Spawner.cs
@@ -2,10 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class Spawner : MonoBehaviour 
 {
-    private void Spawn()
+    [SerializeField]
+    private GameObject _target;
+
+    private void Update() 
+    {
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()) 
+        {
+            Spawn(_target, MouseToWorldPosition());
+        }    
+    }
+
+    private void Spawn(GameObject _objectToSpawn, Vector3 _spawnPos)
 	{
-        
+        if (_objectToSpawn != null && _spawnPos != null)
+        {
+            Instantiate(_objectToSpawn, _spawnPos, Quaternion.identity);
+        }       
 	}
+
+    private Vector3 MouseToWorldPosition() 
+    {
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit _hit;
+
+        if (Physics.Raycast(_ray, out _hit, Mathf.Infinity)) 
+        {
+            GameObject _objectHit = _hit.collider.gameObject;
+            if (_objectHit.tag == "Ground") 
+            {
+                Debug.Log("Spawn object");
+            } 
+            
+            else 
+            {
+                Debug.Log("Spawneable area not found");
+            }
+        }
+
+        return _hit.point;
+    }
 }
