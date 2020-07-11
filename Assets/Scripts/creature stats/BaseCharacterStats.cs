f2c69e3dbc9104ab30fbfdbc58139b26d5ba9dfd
samjerry <jerry.sam@ziggo.nl>
2020-07-11 16:59:33 +0200

    Add a base character stat script. Add a character stats script (which inherits the base script)

---
+++ Assets/Scripts/creature stats/BaseCharacterStats.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseCharacterStats : MonoBehaviour
+{
+
+    public int damageAmount = 10;    // just a default value
+    public int hitPoints = 100;        // just a default value
+
+    public Animator anim;
+
+    //temp variables until we make a combat manager feature
+    GameObject _target;
+
+    protected void Start()
+    {
+        // anim = GetComponent<Animator>();
+        // play spawn animation
+    }
+
+    protected void Attack()
+    {
+        //push damageAmount to the Damage handler script
+        //check if the attacking creature hits.
+        //<name attacking creature> used <Attack> on <Target creature>
+        //then it should calculate damage incl. modifiers
+        //?d? + ? damage
+        //if restistant to damage type -> damage / 2
+        //if immune to damage type -> damage = 0
+        //if vulnerable to damage type -> damage * 2
+        //<name attacking creature> hit <Target creature> for <damage>
+        //<Target creature> has <healthAmount> hitpoints left.
+    }
+
+    protected void DebugStats()
+    {
+        Debug.Log("Health: " + hitPoints);
+        Debug.Log("damage: " + damageAmount);
+    }
+
+    protected void DebugAttack()
+    {
+        Debug.Log("target = " + _target.name);
+    }
+}
---
+++ Assets/Scripts/creature stats/CharacterStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterStats : BaseCharacterStats
+{
+    void Start()
+    {
+        base.Start();    // calls the Start() method in the parent class!
+        SetCharacterStats(10, 3);
+
+        // the DM will get a prompt and must enter all values into the required fields.
+        // all these values will be used in SetCharacterStarts()
+
+        // in here all the stats for the current instance spawn will be assigned e.g. health and damage.
+    }
+
+    void Update()
+    {
+        //do things an enemy child would
+    }
+
+    public void SetCharacterStats(int _health, int _damage)
+    {
+        hitPoints = _health;
+        damageAmount = _damage;
+        base.DebugStats();
+    }
+}
